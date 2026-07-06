2906619401a3360b188cff2dee3e9266a6cd1a3a
Diego-Jose890 <jose.diego1@escolar.ifrn.edu.br>
2026-07-06 13:25:25 -0300

    Retirando código inutilizado

--- model.py
+++ model.py
@@ -20,47 +20,6 @@ def get_conn():
         password=PASSWORD
     )
 
-# def restaurar_backup(arquivo="biblioteca_bck.sql"):
-
-#     if not os.path.exists(arquivo):
-#         print(f"Arquivo '{arquivo}' não encontrado.")
-#         return
-
-#     subprocess.run(
-#         [
-#             "psql",
-#             "-h", HOST,
-#             "-p", PORT,
-#             "-U", USER,
-#             "-d", DB_NAME,
-#             "-f", arquivo
-#         ],
-#         env={**os.environ, "PGPASSWORD": PASSWORD},
-#         check=True
-#     )
-
-#     print("Backup restaurado com sucesso.")
-
-# def banco_vazio():
-#     conn = get_conn()
-#     cur = conn.cursor()
-
-#     cur.execute("""
-#         SELECT EXISTS (
-#             SELECT 1
-#             FROM information_schema.tables
-#             WHERE table_schema = 'public'
-#               AND table_name = 'carro'
-#         )
-#     """)
-
-#     existe = cur.fetchone()[0]
-#
-#    cur.close()
-#    conn.close()
-#
-#    return not existe
-
 def listar():
     conn = get_conn()
     cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
