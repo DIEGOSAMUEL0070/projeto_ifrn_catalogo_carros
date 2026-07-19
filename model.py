4695741a26f902cf986e6b0a5de2f42daacc7ff4
josed <josediegosantos273@gmail.com>
2026-07-18 21:35:13 -0300

    Corrigindo 'model.py' com implementação de criar_tabela completa, corração de 'carro' para 'carros' e 'usuario' para 'usuarios', alteração da identificação de usuário pelo email e não mais pelo nome, adição de listagem separada para marcas, cores, câmbios e combustíveis; além de corração menores dentro do próprio arquivo

--- model.py
+++ model.py
@@ -54,18 +54,125 @@ def criar_tabela():
     cur = conn.cursor()
 
     cur.execute("""
-        CREATE TABLE IF NOT EXISTS 
-
+        CREATE TABLE IF NOT EXISTS marcas (
+            id SERIAL PRIMARY KEY, 
+            nome VARCHAR(100) NOT NULL UNIQUE
+        );
     """)
 
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS cores (
+            id SERIAL PRIMARY KEY,
+            nome VARCHAR(50) NOT NULL UNIQUE
+        );
+    """)
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS cambios (
+            id SERIAL PRIMARY KEY,
+            nome VARCHAR(50) NOT NULL UNIQUE
+        );
+    """)
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS combustiveis (
+            id SERIAL PRIMARY KEY,
+            nome VARCHAR(50) NOT NULL UNIQUE
+        );
+    """)
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS usuarios (
+            id SERIAL PRIMARY KEY,
+            email VARCHAR(150) NOT NULL UNIQUE,
+            senha VARCHAR(255) NOT NULL,
+            nome VARCHAR(100) NOT NULL
+        );
+    """)
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS carros (
+            id VARCHAR(36) PRIMARY KEY,
+            modelo VARCHAR(100) NOT NULL,
+            ano VARCHAR(15) DEFAULT 'Não informado',
+            preco DECIMAL(10, 2) NOT NULL,
+            marca_id INTEGER,
+            cor_id INTEGER,
+            tipo_cambio_id INTEGER,
+            tipo_combustivel_id INTEGER,
+            usuario_id INTEGER,
+
+            FOREIGN KEY (marca_id) REFERENCES marcas(id) ON DELETE SET NULL,
+            FOREIGN KEY (cor_id) REFERENCES cores(id) ON DELETE SET NULL,
+            FOREIGN KEY (tipo_cambio_id) REFERENCES cambios(id) ON DELETE SET NULL,
+            FOREIGN KEY (tipo_combustivel_id) REFERENCES combustiveis(id) ON DELETE SET NULL,
+            FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE SET NULL
+        );
+    """)
+
+    conn.commit()
+
+    cur.execute("SELECT COUNT(*) FROM marcas")
     if cur.fetchone()[0] == 0:
-        cur.executemany()
+        cur.executemany(
+            "INSERT INTO marcas (nome) VALUES (%s)",
+            [
+                ("Chevrolet",),
+                ("Fiat",),
+                ("Ford",), 
+                ("Hyundai",),
+                ("Toyota",), 
+                ("Volkswagen",), 
+                ("Ferrari",)
+            ]
+        )
     
-    cur.execute("SELECT COUNT(*) FROM carro")
+    cur.execute("SELECT COUNT(*) FROM cores")
+    if cur.fetchone()[0] == 0:
+        cur.executemany(
+            "INSERT INTO cores (nome) VALUES (%s)",
+            [
+                ("Branco",),
+                ("Preto",),
+                ("Prata",),
+                ("Cinza",),
+                ("Vermelho",)
+            ]
+        )
+
+    cur.execute("SELECT COUNT(*) FROM cambios")
+    if cur.fetchone()[0] == 0:
+        cur.executemany(
+            "INSERT INTO cambios (nome) VALUES (%s)",
+            [
+                ("Manual",),
+                ("Automático",)
+            ]
+        )
+   
+    
+    cur.execute("SELECT COUNT(*) FROM combustiveis")
+    if cur.fetchone()[0] == 0:
+        cur.executemany(
+            "INSERT INTO combustiveis (nome) VALUES (%s)",
+            [
+                ("Gasolina",),
+                ("Etanol",),
+                ("Diesel",),
+                ("Híbrido",),
+                ("Elétrico",)
+            ]
+        )
+    
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    print("Tabelas criadas e dados iniciais inseridos")
 
 
-def restaurar_backup(arquivo="biblioteca_bck.sql"):
-    if not os.path.exits(arquivo):
+def restaurar_backup(arquivo="banco_dados_carro_bck.sql"):
+    if not os.path.exists(arquivo):
         print(f"Arquivo '{arquivo}' não encontrado.")
         return
     
@@ -93,7 +200,7 @@ def banco_vazio():
             SELECT 1
             FROM information_schema.tables
             WHERE table_schema = 'public'
-              AND table_name = 'carro'
+              AND table_name = 'carros'
         )
     """)
 
@@ -104,10 +211,64 @@ def banco_vazio():
 
     return not existe
 
-def listar():
+def listar_marcas():
     conn = get_conn()
     cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-    cur.execute("SELECT * FROM carro ORDER BY id")
+    cur.execute("SELECT * FROM marcas ORDER BY id")
+    marcas = cur.fetchall()
+    cur.close()
+    conn.close()
+    return marcas
+
+def listar_cores():
+    conn = get_conn()
+    cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    cur.execute("SELECT * FROM cores ORDER BY id")
+    cores = cur.fetchall()
+    cur.close()
+    conn.close()
+    return cores
+
+def listar_cambios():
+    conn = get_conn()
+    cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    cur.execute("SELECT * FROM cambios ORDER BY id")
+    cambios = cur.fetchall()
+    cur.close()
+    conn.close()
+    return cambios
+
+def listar_combustiveis():
+    conn = get_conn()
+    cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    cur.execute("SELECT * FROM combustiveis ORDER BY id")
+    combustiveis = cur.fetchall()
+    cur.close()
+    conn.close()
+    return combustiveis
+
+def listar(marca_id=None, cor_id=None, tipo_cambio_id=None, tipo_combustivel_id=None):
+    conn = get_conn()
+    cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+
+    query = "SELECT * FROM carros WHERE TRUE"
+    valores = []
+
+    if marca_id:
+        query += " AND marca_id = %s"
+        valores.append(marca_id)
+    if cor_id:
+        query += " AND cor_id = %s"
+        valores.append(cor_id)
+    if tipo_cambio_id:
+        query += " AND tipo_cambio_id = %s"
+        valores.append(tipo_cambio_id)
+    if tipo_combustivel_id:
+        query += " AND tipo_combustivel_id = %s"
+        valores.append(tipo_combustivel_id)
+    query += " ORDER BY modelo"
+
+    cur.execute(query, valores)
     carros = cur.fetchall()
     cur.close()
     conn.close()
@@ -116,32 +277,32 @@ def listar():
 def buscar(id):
     conn = get_conn()
     cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-    cur.execute("SELECT * FROM carro WHERE id = %s", (id,))
+    cur.execute("SELECT * FROM carros WHERE id = %s", (id,))
     carro = cur.fetchone()
     cur.close()
     conn.close()
     return carro
 
-def inserir(ano, marca_id, preco, tipo_cambio_id, tipo_combustivel_id, cor_id, modelo, usuario_id):
+def inserir(id, modelo, ano, preco, marca_id, cor_id, tipo_cambio_id, tipo_combustivel_id, usuario_id):
     conn = get_conn()
     cur = conn.cursor()
     cur.execute(
-        """INSERT INTO carro (ano, marca_id, preco, tipo_cambio_id, tipo_combustivel_id, cor_id, modelo, usuario_id) 
-           VALUES (%s, %s, %s, %s, %s, %s, %s, %s)""",
-        (ano, marca_id, preco, tipo_cambio_id, tipo_combustivel_id, cor_id, modelo, usuario_id)
+        """INSERT INTO carros (id, modelo, ano, preco, marca_id, cor_id, tipo_cambio_id, tipo_combustivel_id, usuario_id) 
+           VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)""",
+        (id, modelo, ano, preco, marca_id, cor_id, tipo_cambio_id, tipo_combustivel_id, usuario_id)
     )
     conn.commit()
     cur.close()
     conn.close()
 
-def atualizar(id, ano, marca_id, preco, tipo_cambio_id, tipo_combustivel_id, cor_id, modelo):
+def atualizar(id, modelo, ano, preco, marca_id, cor_id, tipo_cambio_id, tipo_combustivel_id):
     conn = get_conn()
     cur = conn.cursor()
     cur.execute(
-        """UPDATE carro SET ano = %s, marca_id = %s, preco = %s, tipo_cambio_id = %s,
-            tipo_combustivel_id = %s, cor_id = %s, modelo = %s 
+        """UPDATE carros SET modelo = %s, ano = %s, preco = %s, marca_id = %s,
+            cor_id = %s, tipo_cambio_id = %s, tipo_combustivel_id = %s 
         WHERE id = %s""",
-        (ano, marca_id, preco, tipo_cambio_id, tipo_combustivel_id, cor_id, modelo, id)
+        (modelo, ano, preco, marca_id, cor_id, tipo_cambio_id, tipo_combustivel_id, id)
     )
     conn.commit()
     cur.close()
@@ -150,35 +311,35 @@ def atualizar(id, ano, marca_id, preco, tipo_cambio_id, tipo_combustivel_id, cor
 def excluir(id):
     conn = get_conn()
     cur = conn.cursor()
-    cur.execute("DELETE FROM carro WHERE id = %s", (id,))
+    cur.execute("DELETE FROM carros WHERE id = %s", (id,))
     conn.commit()
     cur.close()
     conn.close()
 
-def buscar_usuario_por_login(nome, senha):
+def buscar_usuario_por_login(email, senha):
     conn = get_conn()
     cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-    cur.execute("SELECT * FROM usuario WHERE nome = %s AND senha = %s", (nome, senha))
+    cur.execute("SELECT * FROM usuarios WHERE email = %s AND senha = %s", (email, senha))
     usuario = cur.fetchone()
     cur.close()
     conn.close()
     return usuario
 
-def buscar_usuario_por_nome(nome):
+def buscar_usuario_por_email(email):
     conn = get_conn()
     cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
-    cur.execute("SELECT * FROM usuario WHERE nome = %s", (nome,))
+    cur.execute("SELECT * FROM usuarios WHERE email = %s", (email,))
     usuario = cur.fetchone()
     cur.close()
     conn.close()
     return usuario
 
-def inserir_usuario(nome, senha, tipo_usuario="cliente"):
+def inserir_usuario(email, senha, nome):
     conn = get_conn()
     cur = conn.cursor()
     cur.execute(
-        """INSERT INTO usuario (nome, senha, tipo_usuario) VALUES (%s, %s, %s)""",
-        (nome, senha, tipo_usuario)
+        """INSERT INTO usuarios (email, senha, nome) VALUES (%s, %s, %s)""",
+        (email, senha, nome)
     )
     conn.commit()
     cur.close()
